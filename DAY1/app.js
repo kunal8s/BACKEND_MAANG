04ff7618e58798fc777ee98cb6ac10a6da0000f0
user1 <kunalsharmakunu09@gmail.com>
2026-07-05 13:57:58 +0530

    1

--- DAY1/app.js
+++ DAY1/app.js
@@ -1,6 +1,16 @@
+// const {x,calculatesum} = require('./sum');
+import { multiply } from './multiply.js';
 var a = 10;
 var b = 20;
-console.log(a+b);
-console.log(global);
-console.log(globalThis);
-console.log(globalThis===global);
+
+// console.log(x);
+// console.log(calculatesum(10,20));
+
+console.log(multiply(10,20));
+
+// console.log(a+b);
+// console.log(global);
+// console.log(globalThis);
+// console.log(globalThis===global);
+
+// in one file either u can use require or import not both at once 
---
+++ DAY1/sum.js
@@ -0,0 +1,8 @@
+var x = "Kunal";
+function calculatesum(a,b){
+    return a+b;
+}
+
+module.exports = {
+    x,calculatesum
+}
